76662d9b4f2db71c07a4183c3676d811d791a6aa
adisbladis <adisbladis@gmail.com>
2023-10-31 17:13:44 +1300

    flake: Add nix-github-actions

--- flake.nix
+++ flake.nix
@@ -2,25 +2,27 @@
   description = "Automatically managed Nix packages for Talon Voice";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nix-github-actions.url = "github:nix-community/nix-github-actions";
+  inputs.nix-github-actions.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs.utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, nix-github-actions }:
+  let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    };
+  in
+  {
+    overlays.default = import ./overlay.nix;
+    nixosModules.talon = import ./nixos;
 
-  outputs = { self, nixpkgs, utils }:
-    {
-      overlays.default = import ./overlay.nix;
-      nixosModules.talon = import ./nixos;
-    } //
-    (utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-          };
-        in
-        {
-          packages.default = pkgs.callPackage ./talon.nix { };
-          devShells.default = import ./shell.nix { inherit pkgs; };
-        })
-    );
+    githubActions = nix-github-actions.lib.mkGithubMatrix { inherit (self) checks; };
+
+    checks.x86_64-linux = {
+      talon = self.packages.x86_64-linux.default;
+    };
+
+    packages.x86_64-linux.default = pkgs.callPackage ./talon.nix { };
+    devShells.x86_64-linux.default = import ./shell.nix { inherit pkgs; };
+  };
 }
